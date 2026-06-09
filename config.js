bb713d0b564487a74788aacffb91284b8fcdc011
R2DT <77617514+Roman22022000@users.noreply.github.com>
2026-06-09 23:08:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const NFT_COLLECTIONS = [
+const REQUIRED_COLLECTION =
+"EQD9mm3hx-QvHyNG18gDaaJtSTTQTVVYVjJ6zhonXocj2eyI";
 
-"EQD9mm3hx-QvHyNG18gDaaJtSTTQTVVYVjJ6zhonXocj2eyI",
-
-];
+const COLLECTION_LINK =
+"https://getgems.io/collection/EQD9mm3hx-QvHyNG18gDaaJtSTTQTVVYVjJ6zhonXocj2eyI";
