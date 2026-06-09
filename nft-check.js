f3f07f0750546cd826b530f72d8b139f68678c69
R2DT <77617514+Roman22022000@users.noreply.github.com>
2026-06-09 23:08:42 +0300

    Update nft-check.js

--- nft-check.js
+++ nft-check.js
@@ -1,5 +1,10 @@
 async function checkNFT(address){
 
+try{
+
+status.innerHTML =
+"Проверка NFT...";
+
 const response =
 await fetch(
 `https://tonapi.io/v2/accounts/${address}/nfts?limit=1000`
@@ -8,30 +13,99 @@ await fetch(
 const data =
 await response.json();
 
-let allowed = false;
+console.log(
+"NFT DATA:",
+data
+);
 
-for(const nft of data.nft_items){
+let hasNFT = false;
+
+for(const item of (data.nft_items || [])){
+
+    console.log(
+        "NFT:",
+        item
+    );
 
     const collection =
-    nft.collection;
+    item.collection;
 
     if(!collection)
         continue;
+
+    console.log(
+        "COLLECTION:",
+        collection
+    );
 
     const collectionAddress =
     collection.address || "";
 
     if(
-        NFT_COLLECTIONS.includes(
-            collectionAddress
+        collectionAddress.includes(
+            REQUIRED_COLLECTION
         )
     ){
-        allowed = true;
+
+        hasNFT = true;
         break;
+
     }
 
 }
 
-return allowed;
+if(hasNFT){
+
+    unlockCanvas();
+
+}else{
+
+    lockCanvas();
+
+}
+
+}catch(err){
+
+console.error(err);
+
+status.innerHTML =
+"Ошибка проверки NFT";
+
+lockCanvas();
+
+}
+
+}
+
+function unlockCanvas(){
+
+canvas.style.display =
+"block";
+
+toolbar.style.display =
+"flex";
+
+status.innerHTML =
+"NFT найден. Рисование разрешено.";
+
+}
+
+function lockCanvas(){
+
+canvas.style.display =
+"none";
+
+toolbar.style.display =
+"none";
+
+status.innerHTML =
+`
+Для рисования нужен NFT
+<br><br>
+<a href="${COLLECTION_LINK}"
+target="_blank">
+Перейти к коллекции
+</a>
+`;
 
 }
